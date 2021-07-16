8c0432b3d66e8b049da82c02d92ec9209843b487
Janos Szathmary <janos.szathmary@lightbend.com>
2021-07-16 12:18:36 +0200

    turn off some annoying rules

--- service/index.ts
+++ service/index.ts
@@ -1,9 +1,7 @@
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore
 import { AkkaServerless } from "@lightbend/akkaserverless-javascript-sdk";
 import coreEntity from "./src/core-entity";
 import authEntity from "./src/auth-entity";
 
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore
 new AkkaServerless().addComponent(coreEntity).addComponent(authEntity).start();
